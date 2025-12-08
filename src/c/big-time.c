bd68ec81f790e6a60a3efa0663367f493d952253
Grant Unruh <gunruh@users.noreply.github.com>
2025-12-07 21:55:03 -0600

    Initial working step count. Adding numbers_xs_dark bitmaps.

--- src/c/big-time.c
+++ src/c/big-time.c
@@ -15,10 +15,17 @@ const uint16_t UI_DATE_Y = 4;
 const uint16_t UI_DATE_W = 36;
 const uint16_t UI_DATE_H = 21;
 const uint16_t UI_DATE_SPACING = 1;
-const uint16_t UI_DATE_CONTENT_X = 3;
 const uint16_t UI_DATE_CONTENT_Y = 6;
 
+const uint16_t UI_STEPS_X = 4;
+const uint16_t UI_STEPS_Y = 29;
+const uint16_t UI_STEPS_W = 36;
+const uint16_t UI_STEPS_H = 21;
+const uint16_t UI_STEPS_SPACING = 1;
+const uint16_t UI_STEPS_CONTENT_Y = 6;
+
 static struct tm s_current_time;
+static int s_current_steps = 0;
 
 static Window *s_main_window;
 // static TextLayer *s_text_layer;
@@ -32,7 +39,8 @@ static int s_sun_index_bitmap_height = 3;
 static GBitmap *s_bitmap_sun_index;
 
 static GBitmap *s_bitmap_numbers_lg[10];
-static BitmapInfo s_bitmap_numbers_xs[11];
+static BitmapInfo s_bitmap_numbers_xs_light[11];
+static BitmapInfo s_bitmap_numbers_xs_dark[11];
 
 static BitmapLayer *s_bitmap_layer_background;
 static BitmapLayer *s_bitmap_layer_sun_index;
@@ -43,8 +51,9 @@ static BitmapLayer *s_bitmap_layer_time_m1;
 static BitmapLayer *s_bitmap_layer_time_m1_offset;
 static BitmapLayer *s_bitmap_layer_time_m2;
 
-// Date Layer
 static Layer *s_layer_date;
+
+static Layer *s_layer_steps;
 
 int calculate_string_width_px(char *str, BitmapInfo *bitmapInfoArray, uint16_t spacing_px) {
   int total_width = 0;
@@ -115,11 +124,26 @@ static void layer_date_update_proc(Layer *layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(layer);
   int layer_width = bounds.size.w;
 
-  int date_width = calculate_string_width_px(date_str, s_bitmap_numbers_xs, UI_DATE_SPACING);
+  int date_width = calculate_string_width_px(date_str, s_bitmap_numbers_xs_light, UI_DATE_SPACING);
 
   int starting_x = (layer_width - date_width) / 2;
 
-  draw_string(ctx, date_str, starting_x, UI_DATE_CONTENT_Y, s_bitmap_numbers_xs, UI_DATE_SPACING);
+  draw_string(ctx, date_str, starting_x, UI_DATE_CONTENT_Y, s_bitmap_numbers_xs_light, UI_DATE_SPACING);
+}
+
+static void layer_steps_update_proc(Layer *layer, GContext *ctx) {
+  static char steps_str[] = "00000";
+  snprintf(steps_str, sizeof(steps_str), "%03d", s_current_steps);
+
+  graphics_context_set_compositing_mode(ctx, GCompOpSet);
+  GRect bounds = layer_get_bounds(layer);
+  int layer_width = bounds.size.w;
+
+  int steps_width = calculate_string_width_px(steps_str, s_bitmap_numbers_xs_dark, UI_STEPS_SPACING);
+  
+  int starting_x = (layer_width - steps_width) / 2;
+
+  draw_string(ctx, steps_str, starting_x, UI_STEPS_CONTENT_Y, s_bitmap_numbers_xs_dark, UI_STEPS_SPACING);
 }
 
 static void update_time(struct tm *tick_time) {
@@ -185,13 +209,34 @@ static void update_sun_index(struct tm *tick_time) {
   layer_set_frame(bitmap_layer_get_layer(s_bitmap_layer_sun_index), GRect(sun_index_x, s_sun_index_y, s_sun_index_bitmap_width, s_sun_index_bitmap_height));
 }
 
+static void update_steps() {
+  #if defined(PBL_HEALTH)
+
+  HealthMetric metric = HealthMetricStepCount;
+
+  time_t start = time_start_of_today();
+  time_t end = time(NULL);
+
+  HealthServiceAccessibilityMask mask = health_service_metric_accessible(metric, start, end);
+
+  bool data_available = mask & HealthServiceAccessibilityMaskAvailable;
+  if (data_available) {
+    s_current_steps = (int)health_service_sum_today(metric);
+    layer_mark_dirty(s_layer_steps);
+  }
+
+  #else
+  // Health data not available
+  #endif
+}
+
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   s_current_time = *tick_time;
 
   update_time(tick_time);
   update_date(tick_time);
   update_sun_index(tick_time);
-  // update_steps();
+  update_steps();
   // update_weather();
   // update_daylight();
 }
@@ -212,21 +257,35 @@ static void main_window_load(Window *window) {
   s_bitmap_numbers_lg[8] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_LG_8);
   s_bitmap_numbers_lg[9] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_LG_9_V4);
 
-  s_bitmap_numbers_xs[0] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_0_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
-  s_bitmap_numbers_xs[1] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_1_LIGHT), .width = XS_WIDTH_1, .height = XS_HEIGHT};
-  s_bitmap_numbers_xs[2] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_2_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
-  s_bitmap_numbers_xs[3] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_3_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
-  s_bitmap_numbers_xs[4] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_4_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
-  s_bitmap_numbers_xs[5] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_5_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
-  s_bitmap_numbers_xs[6] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_6_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
-  s_bitmap_numbers_xs[7] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_7_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
-  s_bitmap_numbers_xs[8] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_8_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
-  s_bitmap_numbers_xs[9] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_9_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
-  s_bitmap_numbers_xs[INDEX_DASH] =(BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_DASH_LIGHT), .width = XS_WIDTH_DASH, .height = XS_HEIGHT};
+  // XS Light
+  s_bitmap_numbers_xs_light[0] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_0_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_light[1] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_1_LIGHT), .width = XS_WIDTH_1, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_light[2] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_2_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_light[3] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_3_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_light[4] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_4_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_light[5] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_5_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_light[6] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_6_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_light[7] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_7_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_light[8] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_8_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_light[9] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_9_LIGHT), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_light[INDEX_DASH] =(BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_DASH_LIGHT), .width = XS_WIDTH_DASH, .height = XS_HEIGHT};
+
+  // XS Dark
+  s_bitmap_numbers_xs_dark[0] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_0_DARK), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_dark[1] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_1_DARK), .width = XS_WIDTH_1, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_dark[2] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_2_DARK), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_dark[3] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_3_DARK), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_dark[4] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_4_DARK), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_dark[5] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_5_DARK), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_dark[6] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_6_DARK), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_dark[7] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_7_DARK), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_dark[8] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_8_DARK), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_dark[9] = (BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_9_DARK), .width = XS_WIDTH, .height = XS_HEIGHT};
+  s_bitmap_numbers_xs_dark[INDEX_DASH] =(BitmapInfo) {.gbitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBER_XS_DASH_DARK), .width = XS_WIDTH_DASH, .height = XS_HEIGHT};
 
   // Bitmap Layers
   s_bitmap_layer_background = bitmap_layer_create(bounds);
-  s_bitmap_background = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND_STATIC_V3_2_NO_DATE);
+  s_bitmap_background = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND_STATIC_V3_2_NO_DATE_NO_STEPS);
   bitmap_layer_set_bitmap(s_bitmap_layer_background, s_bitmap_background);
   layer_add_child(root_layer, bitmap_layer_get_layer(s_bitmap_layer_background));
 
@@ -262,6 +321,10 @@ static void main_window_load(Window *window) {
   s_layer_date = layer_create(GRect(UI_DATE_X, UI_DATE_Y, UI_DATE_W, UI_DATE_H));
   layer_set_update_proc(s_layer_date, layer_date_update_proc);
   layer_add_child(root_layer, s_layer_date);
+
+  s_layer_steps = layer_create(GRect(UI_STEPS_X, UI_STEPS_Y, UI_STEPS_W, UI_STEPS_H));
+  layer_set_update_proc(s_layer_steps, layer_steps_update_proc);
+  layer_add_child(root_layer, s_layer_steps);
 }
 
 static void main_window_unload(Window *window) {
@@ -281,9 +344,16 @@ static void main_window_unload(Window *window) {
 
   layer_destroy(s_layer_date);
 
-  array_length = sizeof(s_bitmap_numbers_xs) / sizeof(*s_bitmap_numbers_xs);
+  layer_destroy(s_layer_steps);
+
+  array_length = sizeof(s_bitmap_numbers_xs_light) / sizeof(*s_bitmap_numbers_xs_light);
   for (size_t i = 0; i < array_length; i++) {
-    gbitmap_destroy(s_bitmap_numbers_xs[i].gbitmap);
+    gbitmap_destroy(s_bitmap_numbers_xs_light[i].gbitmap);
+  }
+
+  array_length = sizeof(s_bitmap_numbers_xs_dark) / sizeof(*s_bitmap_numbers_xs_dark);
+  for (size_t i = 0; i < array_length; i++) {
+    gbitmap_destroy(s_bitmap_numbers_xs_dark[i].gbitmap);
   }
 
   gbitmap_destroy(s_bitmap_background);
@@ -310,6 +380,7 @@ static void init() {
   update_time(tick_time);
   update_date(tick_time);
   update_sun_index(tick_time);
+  update_steps();
 
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   tick_handler(tick_time, MINUTE_UNIT);
