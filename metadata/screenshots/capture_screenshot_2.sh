46fdc162d939d3b85044664dc6a6649b3955fe9a
Grant Unruh <gunruh@users.noreply.github.com>
2026-05-29 23:56:19 -0500

    feat(metadata): Add 3rd screenshot. Update capture_screenshot scripts

--- metadata/screenshots/capture_screenshot_2.sh
+++ metadata/screenshots/capture_screenshot_2.sh
@@ -4,13 +4,13 @@
 
 # These are set automatically by the script:
 # Date: Current Date? Hopefully something without a '1'
-# Time: 08:37 (12h)
-# Time Format: 12h (Set by this script)
+# Time: 20:45 (24h)
+# Time Format: 24h (Set by this script)
 
 # Make these manual changes:
 # anticipate.c:
     # Steps: 10372
-    # Conditions: 'RAIN'
+    # Conditions: 'CLEAR' (Moon)
 # index.js:
     # Temp High: 25 C (use F to convert)
     # Temp Current: 23 C (use F to convert)
@@ -31,12 +31,12 @@ pebble build
 
 # Set to just before desired time to capture correct second
 # Update the Date to the current day!
-faketime '2026-05-29 08:36:40' pebble install --emulator $1
+faketime '2026-05-29 20:45:00' pebble install --emulator $1
 # pebble install --emulator $1
 
 # Set time format
-pebble emu-time-format --format 12h
+pebble emu-time-format --format 24h
 
 # Use this command if the emulator is showing in UTC time.
-pebble emu-set-time 03:37:00 && pebble screenshot ${1}_1.png
-# pebble screenshot ${1}_1.png
+pebble emu-set-time 15:45:00 && pebble screenshot ${1}_2.png
+# pebble screenshot ${1}_2.png
